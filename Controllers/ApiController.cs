a4867b828c8c7c193db56bbfb43593ca0e4f12a8
Sergi Muntaner <sergi.mr1@gmail.com>
2019-08-09 20:27:01 +0200

    Push dummy-service-csharp code

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/")]
+[ApiController]
+public class ApiController : ControllerBase
+{
+    public const string VERSION = "1.0";
+
+    // GET: api/
+    [HttpGet("version")]
+    public ActionResult<string> Get(string value)
+    {
+        return  "You are running the version "+ VERSION;
+    }
+}
