71d43083206648d9a4ef5c9a2e0877297fa02300
Sergi Muntaner <sergi.mr1@gmail.com>
2019-09-03 20:28:36 +0200

    Change code to be a version handler

---
+++ Controllers/Version.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+public class Version {
+
+    private const int MAJOR = 1;
+    private const int MINOR = 0;
+    private int PATCH = 0;
+
+    public Version(){
+
+    }
+    
+    public String getVersion() {
+        return String.Format("You are running the version {0}.{1}.{2}", MAJOR, MINOR, PATCH);
+    }
+
+    public String upgradeVersion(){
+        PATCH = PATCH + 1;
+        return String.Format("Successfully upgraded the version to {0}.{1}.{2}", MAJOR, MINOR, PATCH);
+    }
+
+}
---
+++ Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("/")]
+[ApiController]
+public class VersionController : ControllerBase
+{
+    public static Version version = new Version();
+
+    [HttpGet("version")]
+    public ActionResult<string> getVersion()
+    {
+        return version.getVersion();
+    }
+
+    [HttpGet("upgrade")]
+    public ActionResult<string> upgradeVersion()
+    {
+        return version.upgradeVersion();
+    }
+}
